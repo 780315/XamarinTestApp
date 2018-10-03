fdcc76fb28a1eccab792fd7d651df537ae7daf35
Camelia <cameliaflorentina95@gmail.com>
2018-10-03 11:34:12 +0200

    Login page

---
+++ CSmobile/CSmobile/Views/Login.xaml.cs
@@ -0,0 +1,41 @@
+﻿using CSmobile.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CSmobile
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Login : ContentPage
+	{
+		public Login ()
+		{
+            InitializeComponent();
+		}
+
+        void SignInProcedure(object sender, EventArgs e)
+        {
+            User user = new User(Entry_username.Text, Entry_password.Text);
+
+            if(user.CheckInfo())
+            {
+                DisplayAlert("Login", "Login Success", "Oke");
+            }
+            else
+            {
+                DisplayAlert("Login", "Login Not Success, empty username or password", "Oke");
+            }
+
+        }
+
+        /*private void OnLoginButtonClicked(object sender, EventArgs e)
+        {
+
+        } */
+    }
+}
